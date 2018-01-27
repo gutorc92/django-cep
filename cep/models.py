5cb69d5369cd43b22c627612fb2b4383f71c1747
gutorc92 <gutorc@hotmail.com>
2018-01-26 23:51:59 -0200

    Fix
    Signed-off-by: gutorc92 <gutorc@hotmail.com>

--- cep/models.py
+++ cep/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class BRAddres(models.Model):
-    street = modelf.CharField(_('Street'), max_length=200, blank=True)
-    district =modelf.CharField(_('District'), max_length=50, blank=True) 
-    city = modelf.CharField(_('City'), max_length=100, blank=True)
-    state = modelf.CharField(_('State'), max_length=30, blank=True)
+    street = models.CharField(_('Street'), max_length=200, blank=True)
+    district =models.CharField(_('District'), max_length=50, blank=True) 
+    city = models.CharField(_('City'), max_length=100, blank=True)
+    state = models.CharField(_('State'), max_length=30, blank=True)
