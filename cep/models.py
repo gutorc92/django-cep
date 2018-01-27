f9a5a7c00d3e06fa4690d988a00280a0786470dc
gutorc92 <gutorc@hotmail.com>
2018-01-26 23:49:30 -0200

    Model
    Signed-off-by: gutorc92 <gutorc@hotmail.com>

--- cep/models.py
+++ cep/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class BRAddres(models.Model):
+    street = modelf.CharField(_('Street'), max_length=200, blank=True)
+    district =modelf.CharField(_('District'), max_length=50, blank=True) 
+    city = modelf.CharField(_('City'), max_length=100, blank=True)
+    state = modelf.CharField(_('State'), max_length=30, blank=True)
